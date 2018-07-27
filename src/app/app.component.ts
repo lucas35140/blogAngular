1b9fa7b51c7bf62ddd06b41cf1adff3b45e251ce
LucasB <lucas975@hotmail.fr>
2018-07-27 11:08:52 +0200

    finish exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  posts = [
+
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean erat augue, molestie a viverra id, ' +
+        'hendrerit et nibh. Maecenas vitae tincidunt nulla. Integer in nibh sed nunc convallis mattis vitae a mi. ' +
+        'Aenean mattis, elit ac condimentum posuere, nisl purus malesuada diam, in viverra elit magna non massa. ' +
+        'Mauris ullamcorper tempor massa, eget lobortis eros consequat id. Fusce cursus risus mauris, a vestibulum eros mattis vel.',
+      date: new Date()
+
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean erat augue, molestie a viverra id, ' +
+        'hendrerit et nibh. Maecenas vitae tincidunt nulla. Integer in nibh sed nunc convallis mattis vitae a mi. ' +
+        'Aenean mattis, elit ac condimentum posuere, nisl purus malesuada diam, in viverra elit magna non massa. ' +
+        'Mauris ullamcorper tempor massa, eget lobortis eros consequat id. Fusce cursus risus mauris, a vestibulum eros mattis vel.',
+      date: new Date()
+
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean erat augue, molestie a viverra id, ' +
+        'hendrerit et nibh. Maecenas vitae tincidunt nulla. Integer in nibh sed nunc convallis mattis vitae a mi. ' +
+        'Aenean mattis, elit ac condimentum posuere, nisl purus malesuada diam, in viverra elit magna non massa. ' +
+        'Mauris ullamcorper tempor massa, eget lobortis eros consequat id. Fusce cursus risus mauris, a vestibulum eros mattis vel.',
+      date: new Date()
+
+    },
+
+  ];
+
+  constructor() {
+
+  }
+
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+
+  @Input() post: any;
+  love = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setLove(love: boolean) {
+    this.love = love;
+  }
+
+}
